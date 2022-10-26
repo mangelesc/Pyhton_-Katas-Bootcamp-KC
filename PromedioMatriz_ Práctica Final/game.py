29df9f625a6888c3bb2b329c82a3ab4b1171affb
mangelesc <103500921+mangelesc@users.noreply.github.com>
2022-10-26 21:27:22 +0200

    Add files via upload

---
+++ PromedioMatriz_ Práctica Final/game.py
@@ -0,0 +1,9 @@
+from process_list import *
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+if __name__ == "__main__":
+    averages = process_list(numbers)
+    print(numbers)
+    print('AVERAGES')
+    print(averages)
