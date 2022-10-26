b9f90dc9611fe97440816e582be9faf26005cc44
mangelesc <103500921+mangelesc@users.noreply.github.com>
2022-10-26 21:28:50 +0200

    Delete game.py

--- PromedioMatriz_ Práctica Final/game.py
+++
@@ -1,9 +0,0 @@
-from process_list import *
-
-numbers = [1,2,3,4,5,6,7,8,9]
-
-if __name__ == "__main__":
-    averages = process_list(numbers)
-    print(numbers)
-    print('AVERAGES')
-    print(averages)
